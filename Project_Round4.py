3aaf877eed13c22f07750f44a1a097eef5846602
zabaker0 <95827104+zabaker0@users.noreply.github.com>
2021-12-08 17:03:31 -0600

    Update Project_Round4.py

--- Project_Round4.py
+++ Project_Round4.py
@@ -5,6 +5,8 @@
 
 #Round 4
 
+#this is to show the players hand, ignore if N/A
+
 for p in range(players):
     print('{player_list.name}, its your turn')
     
@@ -17,11 +19,12 @@ for p in range(players):
     print('You drew \n')
     player_list.show_hand()
     
-#guessing the suit
+    #guessing the suit
+
     if suit_guess in ['yes','no']:
         if in_hand == True and prediction == 'yes'
         if in_hand == False and prediction == 'no':
-            print('You are win!')
+            print('You win!')
         else:
             print('You lost, take a drink!')
     else:
